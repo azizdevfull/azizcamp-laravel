bbaca16d75e08c99d380aaad3149501ddb5fbadf
azizdevfull <aziz16110904@gmail.com>
2022-11-09 20:03:08 +0500

    fixed some bugs

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -7,7 +7,7 @@
         <form align="center" action="{{ route('projects.store') }}" method="POST" class="mt-5" style="width: 120px; justify-content: center;">
             @csrf
             <input class="form-control" type="text" name="name" required> <br>
-            <textarea name="description"  cols="30" rows="10" required></textarea><br>
+            <textarea name="description"  cols="30" rows="10" minlength="20" required></textarea><br>
             <input class="btn btn-primary" type="submit" value="Create Project">
         </form>
     </div>
